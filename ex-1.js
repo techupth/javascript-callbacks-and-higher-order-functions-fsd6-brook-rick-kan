beac0615ae2804a5a69c702b9e0920364a3b8c81
Kan <kanalovefootball@gmail.com>
2024-05-17 16:49:03 +0700

    Answer:JavaScript-Callbacks-and-Higher-Order-Functions-Assignment

--- ex-1.js
+++ ex-1.js
@@ -1,12 +1,20 @@
 // Exercise #1: For Each Function
 
-function forEach(array, operation) {
-  // Start coding here
+function operation(number, plus) {
+  return number + plus;
+}
+
+function operationArray(callbackOperation, arr, num) {
+  const result = [];
+  for (let n = 0; n < arr.length; n++) {
+    result.push(callbackOperation(arr[n], num));
+  }
+  return result;
 }
 
 const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
-const newEmployeeSalaries = [];
-
-// Using `forEach` function here
+let newEmployeeSalaries = [];
+const numberToPlus = 5000;
+newEmployeeSalaries = operationArray(operation, employeeSalaries, numberToPlus);
 
 console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
--- ex-2.js
+++ ex-2.js
@@ -1,15 +1,38 @@
 //Exercise #2: At Least Five Function
-
-function atLeastFive(array, operation) {
-  // Start coding here
-}
-
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
 const studentScoresRoom2 = [78, 98, 23, 15, 40, 12, 40, 67, 80, 100];
 const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
-// Using `atLeastFive` function here
+function atLeastFive(array, operation) {
+  let count = 0;
+  for (let n = 0; n < array.length; n++) {
+    if (operation(array[n])) {
+      count++;
+    }
+  }
+  return count >= 5;
+}
 
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+function poitnMoreThan70(score) {
+  return score > 70;
+}
+
+let scoreRoom1Result = atLeastFive(studentScoresRoom1, poitnMoreThan70);
+let scoreRoom2Result = atLeastFive(studentScoresRoom2, poitnMoreThan70);
+let scoreRoom3Result = atLeastFive(studentScoresRoom3, poitnMoreThan70);
+
+console.log(scoreRoom1Result);
+console.log(scoreRoom2Result);
+console.log(scoreRoom3Result);
+
+if (scoreRoom1Result === true) {
+  console.log("นักเรียนห้องที่ 1 ผ่านเกณฑ์ ✅");
+}
+
+if (scoreRoom2Result !== true) {
+  console.log("นักเรียนห้องที่ 2 ไม่ผ่านเกณฑ์ ❌");
+}
+
+if (scoreRoom3Result !== true) {
+  console.log("นักเรียนห้องที่ 3 ไม่ผ่านเกณฑ์ ❌");
+}
